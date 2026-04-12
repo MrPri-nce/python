0c9dffcff1a24b2770a81091850e5df180c3c024
MrPri-nce <mrprr1nc33@gmail.com>
2026-04-12 16:38:48 -0500

    Implement television class

--- television.py
+++ television.py
@@ -1,2 +1,53 @@
 class Television:
-    pass
+    min_volume = 0
+    max_volume = 2
+    min_channel = 0
+    max_channel = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__channel = self.min_channel
+        self.__volume = self.min_volume
+
+    def power(self):
+        self.__status = not self.__status
+
+    def mute(self):
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel < self.max_channel:
+                self.__channel += 1
+            else:
+                self.__channel = self.min_channel
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel > self.min_channel:
+                self.__channel -= 1
+            else:
+                self.__channel = self.max_channel
+
+
+    def volume_up(self):
+        if self.__status:
+            if self.__volume < self.max_volume:
+                self.__volume += 1
+            else:
+                self.__volume = self.min_volume
+
+    def volume_down(self):
+        if self.__status:
+            if self.__volume > self.min_volume:
+                self.__volume -= 1
+            else:
+                self.__volume = self.max_volume
+
+    def __str__(self):
+        power_status = 'True' if self.__status else 'False'
+        muted_status = 'True' if self.__muted else 'False'
+        return f'Power = {power_status}, Channel = {self.__channel}, Volume = {self.__volume}, Muted = {muted_status}'
+
